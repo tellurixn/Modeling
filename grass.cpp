86cf9baa696259c6cbcfd3867883cf991f6de98d
tellurian <victor.babaryckin@yandex.ru>
2022-06-24 14:41:16 +0700

    Changen class Grass

--- grass.cpp
+++ grass.cpp
@@ -1,19 +1,28 @@
 #include "grass.h"
 #include <QGraphicsEllipseItem>
 #include <cstdlib>
+#include <ctime>
 
 Grass::Grass() : QObject(), QGraphicsItem(){
-    //int random;
+    srand(time(NULL));
+    int random_x1 = -240 + rand() % 481;
+    int random_y1 = -90 + rand() % 181;
+    int random_x2 = -240 + rand() % 481;
+    int random_y2 = -90 + rand() % 181;
     amount=0;
-    x1 = 0;
-    y1 = 0;
-    x2 = 50;
-    y2 = 50;
+    x1 = random_x1;
+    y1 = random_y1;
+    x2 = random_x2;
+    y2 = random_y2;
 }
 
 void Grass::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(QColor(55,143,46));
     painter->drawEllipse(x1,y1,x2,y2);
+
+
 }
 
 QRectF Grass::boundingRect() const
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "grass.h"
+#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Создаём палитру для тёмной темы оформления
+    QPalette darkPalette;
+
+    // Настраиваем палитру для цветовых ролей элементов интерфейса
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    // Устанавливаем данную палитру
+    qApp->setPalette(darkPalette);
+
     scene = new QGraphicsScene();
     ui->graphicsView->setScene(scene);
 
     grass = new Grass;
     scene->addItem(grass);
+
+    ui->graphicsView->setRenderHint(QPainter::Antialiasing);//сглыживаем линии
 }
 
 MainWindow::~MainWindow()
